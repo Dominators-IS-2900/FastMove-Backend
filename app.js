9084984078e83e417cea4f2fae5f6a6aadeab16c
Sajini Madhushika <86466382+Sajini20@users.noreply.github.com>
2023-04-18 10:03:47 +0530

    update app.js

--- app.js
+++ app.js
@@ -62,3 +62,122 @@ var options={
   });
  });
 
+
+// Get all journeys
+app.get('/journeys', (req, res) => {
+  const sql = 'SELECT * FROM journey';
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Server error');
+      return;
+    }
+    res.status(200).json(result);
+  });
+});
+
+// Add a new journey
+app.post('/journeys', (req, res) => {
+  const { journey_id, bus_no, route_id, income } = req.body;
+  const sql = 'INSERT INTO journey (journey_id, bus_no, route_id, income) VALUES (?, ?, ?, ?)';
+  db.query(sql, [journey_id, bus_no, route_id, income], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Server error');
+      return;
+    }
+    res.status(201).send('Journey added successfully');
+  });
+});
+
+// Update a journey
+app.put('/journeys/:journey_id', (req, res) => {
+  const { bus_no, route_id, income } = req.body;
+  const { journey_id } = req.params;
+  const sql = 'UPDATE journey SET bus_no = ?, route_id = ?, income = ? WHERE journey_id = ?';
+  db.query(sql, [bus_no, route_id, income, journey_id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Server error');
+      return;
+    }
+    res.status(200).send('Journey updated successfully');
+  });
+});
+
+// Delete a jouerney
+app.delete('/journeys/:journey_id', (req, res) => {
+  const { journey_id } = req.params;
+  const sql = 'DELETE FROM journey WHERE journey_id = ?';
+  db.query(sql, [journey_id], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Server error');
+      return;
+    }
+    res.status(200).send('Journey deleted successfully');
+  });
+});
+// Retrieve all conductors
+app.get('/api/conductors', (req, res) => {
+  pool.query('SELECT * FROM conductor', (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal server error');
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+// Retrieve a specific conductor by ID
+app.get('/api/conductors/:id', (req, res) => {
+  const { id } = req.params;
+  pool.query('SELECT * FROM conductor WHERE conductor_id = ?', [id], (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal server error');
+    } else if (rows.length === 0) {
+      res.status(404).send('Conductor not found');
+    } else {
+      res.json(rows[0]);
+    }
+  });
+});
+
+// Create a new conductor
+app.post('/api/conductors', (req, res) => {
+  const { username, password, nic, phone_number, email } = req.body;
+  pool.query('INSERT INTO conductor (username, password, nic, phone_number, email) VALUES (?, ?, ?, ?, ?)', [username, password, nic, phone_number, email], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal server error');
+    } else {
+      const newConductor = {
+        conductor_id: result.insertId,
+        username,
+        password,
+        nic,
+        phone_number,
+        email
+      };
+      res.status(201).json(newConductor);
+    }
+  });
+});
+
+// Update an existing conductor
+app.put('/api/conductors/:id', (req, res) => {
+  const { id } = req.params;
+  const { username, password, nic, phone_number, email } = req.body;
+  pool.query('UPDATE conductor SET username = ?, password = ?, nic = ?, phone_number = ?, email = ? WHERE conductor_id = ?', [username, password, nic, phone_number, email, id], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal server error');
+    } else if (result.affectedRows === 0) {
+      res.status(404).send('Conductor not found');
+    } else {
+      res.status(200).send('Conductor updated successfully');
+    }
+  });
+});
